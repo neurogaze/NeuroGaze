0cf9453f9d05b38dbce2acf91b0272b17a66b209
MikeHi1 <mikeneverd3@gmail.com>
2024-06-19 19:29:28 -0700

    Co-authored-by: Robert Balatbat <robjgb@users.noreply.github.com>

--- static/js/images.js
+++ static/js/images.js
@@ -1,267 +1,100 @@
-import vision from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3";
-const { FaceLandmarker, FilesetResolver, DrawingUtils } = vision;
-const demosSection = document.getElementById("demos");
-const imageBlendShapes = document.getElementById("image-blend-shapes");
-const videoBlendShapes = document.getElementById("video-blend-shapes");
+// constant for API Key
+const API_KEY = "wxFE-eV4eaT1N27OsKq5N8Kt9riF36G9Dy_KSPZenqQ";
+// value for test time duration in seconds
+const TEST_DURATION = 30;
 
-let faceLandmarker;
-let runningMode = "VIDEO";
-let enableWebcamButton= HTMLButtonElement;
-let webcamRunning = false;
-const videoWidth = 480;
-let eyeShapes = {};
-let logInterval = null;
+let images = []; // images array
+let randImgIndex; // randomly selected image index
+let currImgIndex = 0; // current image index
+let corPressedKeys = 0; // correct pressed keys
+let incPressedKeys = 0; // incorrect pressed keys
+let testingPhase = false; // boolean test initiated
+let reactionTime = []; // reactionTimes array
+let corrImgAppears; // correct image appears
+let avgReactionTime; // average reaction time
 
-// Before we can use HandLandmarker class we must wait for it to finish
-// loading. Machine Learning models can be large and take a moment to
-// get everything needed to run.
-async function createFaceLandmarker() {
-  const filesetResolver = await FilesetResolver.forVisionTasks(
-    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
-  );
-  faceLandmarker = await FaceLandmarker.createFromOptions(filesetResolver, {
-    baseOptions: {
-      modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
-      delegate: "GPU"
-    },
-    outputFaceBlendshapes: true,
-    runningMode,
-    numFaces: 1
-  });
+/**
+ * Fetches 10 random images from the Unsplash API.
+ */
+async function fetchImageURLs() {
+  const URL = `https://api.unsplash.com/photos/random?count=10&client_id=${API_KEY}`;
+  const response = await fetch(URL);
+  const data = await response.json();
+  const imgUrls = data.map((img) => img.urls.regular);
 
-}
-createFaceLandmarker();
-
-/********************************************************************
-// Demo 1: Grab a bunch of images from the page and detection them
-// upon click.
-********************************************************************/
-
-// In this demo, we have put all our clickable images in divs with the
-// CSS class 'detectionOnClick'. Lets get all the elements that have
-// this class.
-const imageContainers = document.getElementsByClassName("detectOnClick");
-
-// Now let's go through all of these and add a click event listener.
-for (let imageContainer of imageContainers) {
-  // Add event listener to the child element whichis the img element.
-  imageContainer.children[0].addEventListener("click", handleClick);
+  return imgUrls;
 }
 
-// When an image is clicked, let's detect it and display results!
-async function handleClick(event) {
-  if (!faceLandmarker) {
-    console.log("Wait for faceLandmarker to load before clicking!");
-    return;
-  }
-
-  if (runningMode === "VIDEO") {
-    runningMode = "IMAGE";
-    await faceLandmarker.setOptions({ runningMode });
-  }
-  // Remove all landmarks drawed before
-  const allCanvas = event.target.parentNode.getElementsByClassName("canvas");
-  for (var i = allCanvas.length - 1; i >= 0; i--) {
-    const n = allCanvas[i];
-    n.parentNode.removeChild(n);
-  }
-
-  // We can call faceLandmarker.detect as many times as we like with
-  // different image data each time. This returns a promise
-  // which we wait to complete and then call a function to
-  // print out the results of the prediction.
-  const faceLandmarkerResult = faceLandmarker.detect(event.target);
-  const canvas = document.createElement("canvas");
-  canvas.setAttribute("class", "canvas");
-  canvas.setAttribute("width", event.target.naturalWidth + "px");
-  canvas.setAttribute("height", event.target.naturalHeight + "px");
-  canvas.style.left = "0px";
-  canvas.style.top = "0px";
-  canvas.style.width = `${event.target.width}px`;
-  canvas.style.height = `${event.target.height}px`;
-
-  event.target.parentNode.appendChild(canvas);
-  const ctx = canvas.getContext("2d");
-  const drawingUtils = new DrawingUtils(ctx);
-  for (const landmarks of faceLandmarkerResult.faceLandmarks) {
-
-    drawingUtils.drawConnectors(
-      landmarks,
-      FaceLandmarker.FACE_LANDMARKS_RIGHT_IRIS,
-      { color: "#FF3030" }
-    );
-    drawingUtils.drawConnectors(
-      landmarks,
-      FaceLandmarker.FACE_LANDMARKS_LEFT_IRIS,
-      { color: "#30FF30" }
-    );
-  }
-  drawBlendShapes(imageBlendShapes, faceLandmarkerResult.faceBlendshapes);
-}
-
-/********************************************************************
-// Demo 2: Continuously grab image from webcam stream and detect it.
-********************************************************************/
-
-const video = document.getElementById("webcam");
-const canvasElement = document.getElementById(
-  "output_canvas"
-);
-
-const canvasCtx = canvasElement.getContext("2d");
-
-// Check if webcam access is supported.
-function hasGetUserMedia() {
-  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
-}
-
-// If webcam supported, add event listener to button for when user
-// wants to activate it.
-if (hasGetUserMedia()) {
-  enableWebcamButton = document.getElementById(
-    "webcamButton"
-  );
-  enableWebcamButton.addEventListener("click", enableCam);
-} else {
-  console.warn("getUserMedia() is not supported by your browser");
-}
-
-// Enable the live webcam view and start detection.
-function enableCam(event) {
-  if (!faceLandmarker) {
-    console.log("Wait! faceLandmarker not loaded yet.");
-    return;
-  }
-
-  if (webcamRunning === true) {
-    webcamRunning = false;
-    enableWebcamButton.innerText = "ENABLE PREDICTIONS";
-    stopLogging();
-  } else {
-    webcamRunning = true;
-    enableWebcamButton.innerText = "DISABLE PREDICTIONS";
-     startLogging() ;
-  }
-
-  // getUsermedia parameters.
-  const constraints = {
-    video: true
-  };
-
-  // Activate the webcam stream.
-  navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-    video.srcObject = stream;
-    video.addEventListener("loadeddata", predictWebcam);
+/**
+ * Show the instruction at the start up screen.
+ */
+function showInitialInstructions() {
+  swal({
+    title: "ADHD Visual Attention Test",
+    text: "The computer will generate 10 random images. It will pick one image which you will need to press the spacebar when you see. Do not press the spacebar if a different image appears.",
+    icon: "info",
+    button: "Next",
+  }).then((isConfirm) => {
+    if (isConfirm) {
+      startingTestInstructions();
+    }
   });
 }
 
-let lastVideoTime = -1;
-let results = undefined;
-const drawingUtils = new DrawingUtils(canvasCtx);
-async function predictWebcam() {
-  // Set the canvas height and width to be the same as the video.
-  const radio = video.videoHeight / video.videoWidth;
-  video.style.width = videoWidth + "px";
-  video.style.height = videoWidth * radio + "px";
-  canvasElement.style.width = videoWidth + "px";
-  canvasElement.style.height = videoWidth * radio + "px";
-  canvasElement.width = video.videoWidth;
-  canvasElement.height = video.videoHeight;
-  canvasElement.style.display = "none";
-  video.style.display = "none";
+/**
+ * This is instructions for the selected image.
+ */
+async function startingTestInstructions() {
+  images = await fetchImageURLs(); // Array of 10 images
+  // Set randImgIndex to one of the images randomly
+  randImgIndex = Math.floor(Math.random() * images.length);
+  console.log(randImgIndex, images[randImgIndex]);
 
-  // Now let's start detecting the stream.
-  if (runningMode === "IMAGE") {
-    runningMode = "VIDEO";
-    await faceLandmarker.setOptions({ runningMode: runningMode });
-  }
-  let startTimeMs = performance.now();
-  if (lastVideoTime !== video.currentTime) {
-    lastVideoTime = video.currentTime;
-    results = faceLandmarker.detectForVideo(video, startTimeMs);
-  }
-  if (results.faceLandmarks) {
-    for (const landmarks of results.faceLandmarks) {
-        //Draw the iris landmarks
-        drawingUtils.drawConnectors(
-            landmarks,
-            FaceLandmarker.FACE_LANDMARKS_RIGHT_IRIS,
-            { color: "#FF3030" }
-        );
-        drawingUtils.drawConnectors(
-            landmarks,
-            FaceLandmarker.FACE_LANDMARKS_LEFT_IRIS,
-            { color: "#30FF30" }
-            );
-        }
-  }
-  drawBlendShapes(videoBlendShapes, results.faceBlendshapes);
-
-  // Call this function again to keep predicting when the browser is ready.
-  if (webcamRunning === true) {
-    window.requestAnimationFrame(predictWebcam);
-  }
-}
-
-function drawBlendShapes(el, blendShapes) {
-    if (!blendShapes.length) {
-        return;
+  swal({
+    title: "Your Image",
+    text: "This is the image. Make sure to press the spacebar when you see it again.",
+    icon: images[randImgIndex],
+    button: "Start Test",
+  }).then((isConfirm) => {
+    if (isConfirm) {
+      testingPhase = true // set testingPhase to true
+      showRandomImage(); // call random image function
+      setTimeout(endTest, TEST_DURATION * (1000)); // setTimeout to end test after test duration
+      
     }
-
-    // Filter out only the blend shapes related to the iris
-    const irisBlendShapes = blendShapes[0].categories.filter(shape => {
-        return shape.categoryName.includes("eyeLookIn") || shape.categoryName.includes("eyeLookOut");
-    });
-
-    let htmlMaker = "";
-    // Store the current iris blend shapes
-    eyeShapes =  irisBlendShapes;
-    irisBlendShapes.forEach(shape => {
-        // Store the current shape
-
-//        if(shape.score>0.5){
-//            console.log(shape.categoryName, shape.score);
-//        }
-        // Create the HTML to display the blend shape value
-        htmlMaker += `
-            <li class="blend-shapes-item">
-                <span class="blend-shapes-label">${shape.displayName || shape.categoryName}</span>
-                <span class="blend-shapes-value" style="width: calc(${+shape.score * 100}% - 120px)">${(+shape.score).toFixed(4)}</span>
-            </li>
-        `;
-    });
-
-    el.innerHTML = htmlMaker;
-}
-function logIrisLandmarks() {
-    for (let i = 0; i < eyeShapes.length; i++) {
-        const shape = eyeShapes[i];
-        // Check if the user is looking left or right
-        if (shape.score > 0.3) {
-            console.log(shape);
-            if (shape.categoryName == "eyeLookInLeft" || shape.categoryName == "eyeLookOutRight") {
-                 alert('User is looking right');
-            } else if (shape.categoryName == "eyeLookOutLeft" || shape.categoryName == "eyeLookInRight") {
-                 alert('User is looking left');
-            }
-            break; // Exit the loop
-        }
-    }
+  })
 }
 
-// Function to start the logging interval
-function startLogging() {
-    if (logInterval) {
-        clearInterval(logInterval);
-    }
-    // Log the iris landmarks every second
-    logInterval = setInterval(logIrisLandmarks, 1000);
+/**
+ * Test start
+ */
+function showRandomImage(){
+  if (!testingPhase) return;
+  let currentImage = document.getElementById("currentImg");
+  currImgIndex = Math.floor(Math.random() * images.length) // random current index 
+  currentImage.src = images[currImgIndex]// set currentImage.src to random current index inside images array
+
+  setTimeout(showRandomImage, Math.random() * 1000 + 1000);
 }
 
-// Function to stop the logging interval
-function stopLogging() {
-    if (logInterval) {
-        clearInterval(logInterval);
-        logInterval = null;
-    }
+/**
+ * Ends the test
+ */
+function endTest() {
+  testingPhase = false;
 }
+
+
+/**
+ * Event listener for keypress event
+ */
+document.addEventListener("keypress", function(event) {
+  if (testingPhase){
+    console.log('Key pressed:', event.code);
+  }
+});
+
+
+showInitialInstructions();
+
+
